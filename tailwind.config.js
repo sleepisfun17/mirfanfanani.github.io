c5930d731f67a3a4ac18f6364691434cb7f9042e
M Irfan Fanani <36741093+sleepisfun17@users.noreply.github.com>
2022-08-15 12:04:52 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F43F5E",
+        dark: "#475569",
+      },
+    },
+  },
+  plugins: [],
+};
